d55bbf9e369d9a8cb80fc714b4b8962be61d07b5
Marc Lammers <marc@lammers.dev>
2025-12-24 12:11:34 +0100

    fix: disable checkJs to avoid checking build artifacts in CI

--- .eslintrc.js
+++ .eslintrc.js
@@ -63,6 +63,7 @@ module.exports = {
             rules: {
                 '@typescript-eslint/explicit-function-return-type': 'off',
                 '@typescript-eslint/no-var-requires': 'off',
+                '@typescript-eslint/ban-ts-comment': 'off',
             },
         },
     ],
